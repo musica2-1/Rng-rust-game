8b9175456542fdf41a534400651854d03d065e74
Musica <fotaku54@gmail.com>
2026-07-14 10:27:48 -0300

    Revise user prompts and messages for guessing game
    Updated print statements for better engagement and clarity.

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,25 @@ use std::io;
 use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
+    println!("Guess the numbaa!!!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     loop {
-        println!("Please input your guess.");
+        println!("Please, input your guess.");
 
         let mut guess = String::new();
 
         io::stdin()
             .read_line(&mut guess)   //-Le apenas input string
-            .expect("Failed to read line");
+            .expect("deu erro bro");
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
-        println!("You guessed: {guess}");
+        println!("You guessou: {guess}");
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
